cc30431a85bb5ed763ba7d7981372e21e6e7e390
= <=>
2023-10-28 15:35:56 +0200

    Con este fichero elegiremos el idioma con el que quedremos ver el login y el registro ya que dependiando el idioma elegido se creara una cookie de idioma o otra

--- Eric/Practica5/views/seleccio_idioma.php
+++ Eric/Practica5/views/seleccio_idioma.php
@@ -1,3 +1,14 @@
+<?php
+if (
+    isset($_COOKIE['sel_idioma'])
+) {
+    $pagina_anterior = $_SERVER['HTTP_REFERER'];
+    header("Location: $pagina_anterior"); 
+}
+
+else{
+
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -5,19 +16,21 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Seleccio idioma</title>
 </head>
-<body>
+<body>  
     <table width="25%" border="0" align="center">
         <tr>
             <td>Escull Idioma</td>
         </tr>
         <tr>
             <td align="center"><a href="../cookie/crearCookie.php?idioma=es">
-            <img src="../austria/Descargas/spain-500x500.jpg" width="90" height="60"></a><h1>SPAIN</h1></td>
+            <img src="../img/español.jpeg" width="90" height="60"></a><h1>SPAIN</h1></td>
             <td align="center"><a href="../cookie/crearCookie.php?idioma=en">
-            <img src="../austria/Descargas/english.jpg" width="90" height="60"></a><h1>ENGLISH</h1></td>    
+            <img src="../img/english.jpg" width="90" height="60"></a><h1>ENGLISH</h1></td>    
             <td align="center"><a href="../cookie/crearCookie.php?idioma=ca">
-            <img src="../Descargas/catalanes.jpg" width="90" height="60"></a><h1>CATALAN</h1></td>
+            <img src="../img/catalanes.jpg" width="90" height="60"></a><h1>CATALAN</h1></td>
         </tr>   
     </table>
+
 </body>
+<?php } ?>
 </html>
