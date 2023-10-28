e9a925a35f4233cd67e09be6847b0c7222246d07
= <=>
2023-10-28 15:33:05 +0200

    Aqui borramos la cookie

--- Eric/Practica5/cookie/deleteCookie.php
+++ Eric/Practica5/cookie/deleteCookie.php
@@ -1,5 +1,9 @@
 <?php
-    setcookie("sel_idioma","",time()-1);
-    header('Location: ../views/seleccio_idioma.php');
-
+if (isset($_COOKIE['sel_idioma'])) {
+    setcookie("sel_idioma", "", time() - 1);
+    $pagina_anterior = $_SERVER['HTTP_REFERER'];    //Aqui creo una variable que guarda la URL de la pagina desde donde vengo 
+    header("Location: $pagina_anterior");
+} else {
+    echo '<h1>La cookie no existe.</h1>';
+}
 ?>
